912f44a64f5f4f87fc781e880762f6085ddd4242
joe laptop vm <iamthejoe@blueyonder.co.uk>
2018-10-18 23:44:07 +0100

    Added function for returning standard base62 characters

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # custom_base62
 A python object for using the base62 number system, allowing for custom character orders 
+
+# usage
+
+```python3
+# import object
+from custom62 import CustomBase62
+
+# generate a new unique base62 signature
+sig = CustomBase62.sig_gen()
+
+# output : 'Fsj50wIB91VkEAlrzH6PxybX2c73tSThZnGCqKmJfNUgpYQvLMauRdeWDoi48O'
+
+# Retrieve standard base62 characters
+CustomBase62.base62_std()
+
+# output : '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# create new CustomBase62 object and initiate with our signature
+cb = CustomBase62(sig)
+
+# encode an int from our unique signature
+cb.encode(200)
+
+# output : '5l'
+
+# decode an int from our unique signature
+cb.decode('5l')
+
+# output : 200
+
+# retreive signature from current object
+cb.cur_sig()
+
+# output : 'Fsj50wIB91VkEAlrzH6PxybX2c73tSThZnGCqKmJfNUgpYQvLMauRdeWDoi48O'
+```
+
+# known limitations
+With very large ints ( > 1,000,000,000,000,000,000), float will run into issues and some tweaking of the object may be required. You can use float128 from nmpy, however it is probably unlikely that such numbers will be acheived
--- custom62.py
+++ custom62.py
@@ -2,42 +2,38 @@ from random import choice
 
 
 class CustomBase62:
-    """
-    Encode and decode int values from a custom base64 signature
+    """Encode and decode int values from a custom base64 signature
     Use [0-9] [a-z] [A-Z] only
     Must only use 1 of each character
     """
-    
+
     def __init__(self, signature):
-        """
-        Initiate object.
+        """Initiate object.
         Takes 1 parameter; signature.
         """
         if signature:
             self._signature = self.test_signature(signature)
         else:
             raise ValueError('62 character signature has not been provided')
-        
+
         self._sig_dict = self._build_dict(self._signature)
-        
-    
+
+
     def cur_sig(self):
-        """
-        return current signature for object
+        """return current signature for object
         """
         return self._signature
-    
-    
+
+
     def encode(self, integer):
+        """Encode integer to base62 using the current object signature
         """
-        Encode integer to base62 using the current object signature
-        """
-        
+
         # test input parameter
         if not type(int()) == type(integer):
             raise ValueError('\'{prov}\' is not an integer')
-        
-        
+
+
         # ns is split of float value from division
         # ns[0] is whole number left of division
         # ns[1] is remainder of division
@@ -56,97 +52,98 @@ class CustomBase62:
             next = ''
 
         return next + self._signature[integer]
-    
-    
+
+
     def decode(self, base62_str):
-        """
-        Decode int value from base62 string.
+        """Decode int value from base62 string.
         """
 
         # length of string - determine power values
         hex_len = len(base62_str) - 1
-        
+
         # return int value
         rtn_int = 0
-        
+
         # loop through hex string
         for h in base62_str:
-            
+
             # calculate value of hex
             rtn_int += int(self._sig_dict[h] * pow(62, hex_len))
-            
+
             # deduct 1 from length (for pow calculation)
             hex_len -= 1
-            
+
         return  rtn_int
-        
-            
+
+    @staticmethod
+    def base62_std():
+        """Return standard values for base62
+        uses: standard sig, internal object functions
+        """
+        return '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
     @staticmethod
     def _build_dict(signature):
-        """
-        return dictionary of signature characters.
+        """return dictionary of signature characters.
         Value of each character is the corresponding int value
         """
-        
+
         # define dictionary to return
         rtn_dict = dict()
-        
+
         # loop through signature and append dictionary
         for i in range(62):
             rtn_dict[signature[i]] = i
-        
+
         return rtn_dict
-        
-    
+
+
     @staticmethod
     def test_signature(signature):
-        """
-        test signature for:
+        """Test signature for:
         uniqueness
         correct values
         correct amount of values
         """
-        
-        # string containing one of each valid characters available
+
+        # get standard base62 characters for testing
         skel = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        
+
         # set to validate uniqueness of signauture value
         signature_set = set()
-        
+
         if not len(signature) == 62:
             raise ValueError('Signature is not 62 characters long')
-        
+
         for s in signature:
-            
+
             # add letter to set for verification
             signature_set.add(s)
-            
-            # check for valid signature character 
+
+            # check for valid signature character
             if not s in skel:
                 raise ValueError(
                     '\'{a}\' is not a valid signature character'.format(a=s)
                     )
-            
+
         if not len(signature_set) == 62:
             raise ValueError('Signature values are not unique')
         return signature
-        
-        
+
+
     @staticmethod
     def sig_gen():
+        """Generate a random "signature" from a complete character list
         """
-        Generate a random "signature" from a complete character list
-        """
-        # standard collection of values
+
+        # standard values for reduction
         skel = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        
+
         rtn_sig = ''
-        
+
         while bool(skel) == True:
             ch = choice(skel)
             skel = skel.replace(ch, '')
             rtn_sig = rtn_sig + ch
-        
+
         return rtn_sig
-    
-            
